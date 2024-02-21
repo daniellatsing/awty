4275bfcce212b380ba97e473e0ce713ebf230215
Daniella <daniellatsing@gmail.com>
2024-02-20 19:03:33 -0800

    possible completion - consistent sms perm denied

--- app/src/main/java/edu/uw/ischool/dtsing/arewethereyet/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/dtsing/arewethereyet/MainActivity.kt
@@ -6,16 +6,22 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony
 import android.telephony.PhoneNumberUtils
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
+import android.Manifest
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +30,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var intervalEditText: EditText
     private lateinit var startButton: Button
     private var receiver : BroadcastReceiver? = null
+    private var SMS_PERMISSION_REQUEST_CODE: Int = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,16 +52,61 @@ class MainActivity : AppCompatActivity() {
                         intervalEditText.text.toString().toInt(),
                         messageEditText.text.toString()
                     )
+                    sendSMS()
                 } else { // Otherwise, pop a Toast on why the application cannot start
-                    Toast.makeText(this, "Please fill out all fields correctly", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this,
+                        "Please fill out all fields correctly",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             } else { // Otherwise, stop the service
                 stopService(Intent(this, startButton::class.java))
                 Log.i("Stop", "Stop button has been clicked")
                 startButton.text = getString(R.string.start)
+            }
+        }
+        checkForSmsPermission()
+    }
+
+    private fun checkForSmsPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) !=
+            PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.SEND_SMS),
+                SMS_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            startButton.isEnabled = true
         }
     }
-}
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            SMS_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "SMS Permission Granted",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    startButton.isEnabled = true
+                } else {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "SMS Permission Denied",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    startButton.isEnabled = false
+                }
+            }
+        }
+    }
 
     private fun legitimateValues(): Boolean {
         // Message should be filled out to be legit
@@ -87,12 +139,17 @@ class MainActivity : AppCompatActivity() {
 
         val intent = Intent("edu.uw.ischool.dtsing.ALARM")
 
-        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
 
         if (receiver == null) {
             receiver = object : BroadcastReceiver() {
                 override fun onReceive(context: Context?, intent: Intent?) {
-                    val toastMessage = "${phoneNumber}: ${message}"
+                    val toastMessage = "${phoneNumber}: $message"
 
                     // Toast messages using the format: "(425) 555-1212: Are we there yet?".
                     // Toast.makeText(this, "${phoneNumber}: Are we there yet?", Toast.LENGTH_SHORT).show()
@@ -100,12 +157,27 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             val filter = IntentFilter("edu.uw.ischool.dtsing.ALARM")
-            registerReceiver(receiver, filter)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                registerReceiver(receiver, filter, RECEIVER_NOT_EXPORTED)
+            }
         }
 
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time, intervalMillis, pendingIntent)
+    }
 
+    private fun sendSMS() {
+        checkForSmsPermission()
 
+        val defaultSmsPackageName = Telephony.Sms.getDefaultSmsPackage(this)
+
+        val sendIntent = Intent(Intent.ACTION_SEND)
+        sendIntent.setType("text/plain")
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Hello there!")
+
+        if (defaultSmsPackageName != null) {
+            Log.i("MainActivity","defaultSmsPackageName is $defaultSmsPackageName")
+            sendIntent.setPackage(defaultSmsPackageName)
+        }
+        startActivity(sendIntent)
     }
 }
-
